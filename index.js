17ed7092ee5bf6ad075a61d2204dab26fd1c9d8e
Chris Tullier <christullier@gmail.com>
2019-01-25 16:23:33 -0600

    fix db page view issue

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ express()
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/dbb', results );
+      res.render('pages/db', results );
         client.release();
     } 
     catch (err) {
