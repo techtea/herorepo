94b37d852e8dae5239f411b814d8accc04c38965
Chris Tullier <christullier@gmail.com>
2019-01-25 15:52:41 -0600

    add travel path

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/travel', (req, res) => res.showTravel())
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -27,3 +28,18 @@ express()
     }
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  showTravel = () => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+        client.release();
+    } 
+    catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+  }
+    
