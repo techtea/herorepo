a8b1a29d90a57a3753003ebb5c6567dff0845c4c
Chris Tullier <christullier@gmail.com>
2019-01-25 15:54:38 -0600

    move showTravel

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const pool = new Pool({
   ssl: true
 });
 
+
+showTravel = () => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+      client.release();
+  } 
+  catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+  }
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -29,17 +44,4 @@ express()
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  showTravel = () => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-        client.release();
-    } 
-    catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-  }
     
