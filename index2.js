fbfca20945ba33a58cbdcae7cd67951e265125fa
Chris Tullier <christullier@gmail.com>
2019-01-25 15:57:12 -0600

    .

--- index2.js
+++ index2.js
@@ -9,11 +9,26 @@ const pool = new Pool({
 });
 
 
+showTravel = () => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+      client.release();
+  } 
+  catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+  }
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/travel', (req, res) => res.showTravel())
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -26,7 +41,7 @@ express()
         console.error(err);
         res.send("Error " + err);
     }
-})
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
     
